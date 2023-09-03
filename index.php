cb19fd3fbcb5aa682bf3adcd2abb408144346f3a
domingosjadriano <domingosadriano123@gmail.com>
2023-09-03 09:19:21 -0300

    adiconando o index.php

---
+++ index.php
@@ -0,0 +1,216 @@
+'<!DOCTYPE html>
+<html>
+<head>
+	<title>Bem Vindo a House Burguer</title>
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<meta charset="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+	<meta charset="utf-8" />
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	<meta charset="description" content="Site de Fast Food House Burguer">
+	<meta charset="keywords" content="house,burguer,mario,vicente,humburguer, Hamburguer">
+	<meta charset="author" content="JamAdri" />
+	
+</head>
+<body>
+
+<section class="principal">
+	<div class="container">
+		<div class="box">
+			<header>
+				<div class="logo">
+					<img alt="logo-houseBurguer" title="House Burguer Mario Vicente" src="imagens/lg0.png" />
+				</div><!--logo-->
+				<div class="clean"></div>
+				<nav class="desktop">
+					<ul>
+						<li style="font-size: 20px; border-bottom: 2px solid orange"><a href="#">HOME</a></li>
+						<li><a href="#">FASTFOODS</a></li>
+						<li><a href="#">SOBRE</a></li>
+					</ul>
+					<div class="clean"></div>
+				</nav><!--desktop-->
+
+				<nav class="mobile">
+					<i class="fa fa-bars"></i>
+					<ul>
+						<li style="font-size: 20px; border-bottom: 2px solid orange"><a href="#">HOME</a></li>
+						<li><a href="#">FASTFOODS</a></li>
+						<li><a href="#">SOBRE</a></li>
+					</ul>
+				</nav>
+				<div class="clean"></div>
+			</header>
+
+			<div class="chamada">
+				<div class="texto">
+					<h1>HOUSE</h1>
+					<h2>BURGUER</h2>
+					<p>O melhor Hamburguer à distância de um click</p>
+					<h4>Seja Bem Vindo</h4>
+				</div><!--texto-->
+			</div><!--chamada-->
+		
+		</div><!--box-->
+	</div><!--container-->
+</section><!--principal-->
+
+
+
+
+
+
+<section class="producto">
+	<div class="container">
+		<div class="box">
+
+			<div class="texto">
+				<h2>Conheça os fastfood mais solicitados</h2>
+				<p>Não espere mais, faça ja o seu pedido!</p>
+			</div><!--texto-->
+
+			<div class="flex2">
+				<div class="info" id="curva">
+					<div class="flex3">
+						<div class="img" style="background-image: url('imagens/hamburguer.jpg');"></div>
+						<div class="txt">
+							<h2>HAMBURGUER SIMPLES</h2>
+							<p>Preço de Promoção</p>
+							<p><span>1.000 Kz</span> </p>
+						</div><!--txt-->
+					</div><!--flex3-->
+				</div><!--info-->
+
+				<div class="item-info">
+					<div class="flex3">
+						<div class="img" style="background-image: url('imagens/hamburguerComposto.jpg');"></div>
+						<div class="txt">
+							<h2>HAMBURGUER COMPOSTO</h2>
+							<p>Preço de Promoção</span></p>
+							<p><span>1.300 Kz</span></p>
+						</div><!--txt-->
+					</div><!--flex3-->
+				</div><!--info-->
+			</div><!--flex2-->
+		
+			<div class="flex2">
+				<div class="info">
+					<div class="flex3">
+						<div class="txt">
+							<h2>CACHORRO COMPOSTO</h2>
+							<p>Preço de Promoção</p>
+							<p><span>900 Kz</span></p>
+						</div><!--txt-->
+						<div class="img" style="background-image: url('imagens/cachorro.jpg');"></div>
+					</div><!--flex3-->
+				</div><!--info-->
+
+				<div class="item-info">
+					<div class="flex3" id="curva">
+						<div class="txt">
+							<h2>CACHORRO SIMPLES</h2>
+							<p>Preço de Promoção</p>
+							<p><span>800 Kz</span></p>
+						</div><!--txt-->
+						<div class="img" style="background-image: url('imagens/cachorros.jpg');"></div>
+					</div><!--flex3-->
+				</div><!--info-->
+			</div><!--flex2-->
+		</div><!--container-->
+	</div><!--box-->
+</section><!--four-->
+
+
+
+<div class="clean"></div>
+
+<section class="chamada1">
+	<div class="box">
+		<div class="center">
+			<div class="flex4">
+				<div class="texto-chamada1">
+					<h2>Faça o seu pedido agora mesmo</h2>
+					<p>Equipe de atendimento desponível 24h/24h</p>
+				</div><!--texto-chamada1-->
+				<div class="botao-chamada1">
+					<div class="texto-btn">
+						<a href="#">Acessar</a>
+					</div><!--texto-btn-->
+				</div><!--botao-chamada1-->
+			</div><!--flex4-->
+		</div><!--center-->
+	</div><!--box-->
+</section><!--chamada1-->
+
+
+
+<div class="clean"></div>
+
+<section class="paralax">
+	<div class="container">
+		<div class="box">
+			<h2>O que os nossos clientes dizem</h2>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volelit esse molestie consequat, vel illum dolore.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+
+			<h3><a href="#" style="color: #077c94;"><i class="fab fa-facebook"></i></a> Samara Miranda</h3>
+		</div><!--box-->
+	</div><!--container-->
+</section><!--paralax-->
+
+<div class="clean"></div>
+
+<div class="Botao"><a href="#">FALE CONOSCO</a></div>
+
+
+<footer>
+	<div class="container">
+		<div class="flex2">
+			<div class="endereco1">
+				<h2>Contactos:</h2>
+				<h3><a href="#" style="color: #077c94"><i class="fab fa-facebook"></i></a> Oficial House Burguer</h3>
+				<h3><a href="#" style="color: #00cd00"><i class="fab fa-whatsapp"></i></a> (+244) 999 999 999</h3>
+				<h3><a href="#" style="color: #077c94"><i class="fab fa-facebook"></i></a> Mário Vicente Oficial</h3>
+			</div><!--endereco1-->
+
+			<div class="endereco2">
+				<h2>Horário de Atendimento</h2>
+				<h3>Segunda à Sexta, 9h às 22h</h3>
+				<h3>Sábado e Domingo, 9h às 23h</h3>
+				<h3>Fferiados, 9h às 23h</h3>
+			</div><!--endereco2-->
+		</div><!--flex2-->
+		
+		<div class="last-footer">
+			<p>Distrito Urbano da Estalagem, Viana, Rua do Coelho, Luanda - Angola | E-mail: houseburguer@gmail.com</p>
+			<p> C 2023 Desenvolvido por <strong>Domingos JamAdri</strong> Designer HTML5 & CSS3</p>
+		</div><!--last-footer-->
+	</div><!--container-->
+</footer>
+
+<div class="clean"></div>
+
+		<script src="js/jquery.js"></script>
+			<script>
+				$(function(){
+					$('nav.mobile i').click(function(){
+						var el = $(this).parent().find('ul');
+						if (el.is(':visible') == false) {
+							el.fadeIn();
+						}else{
+							el.fadeOut();
+						}
+					})
+				})
+			</script>
+
+
+
+
+<footer>
+	
+</footer>
+
+
+</body>
+</html>'
